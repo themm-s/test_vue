aef910c31f29a0510ede32b1fdcceaace3c370c4
themm-s <maximusnewera@mail.ru>
2024-06-13 22:01:47 +0600

    test

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,11 @@ import CardList from './components/CardList.vue';
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
